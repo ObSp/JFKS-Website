8b845145f63c4a6f1f1e20121fcbb87c0a1cc25d
ObSp <150492054+ObSp@users.noreply.github.com>
2024-09-07 08:41:45 -0400

    stuff

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,18 @@
+import { colors, lightBackgroundColor } from "./lightColors.js";
+
 const id = function(id){
     return document.getElementById(id)
 }
 
 const searchInput = id("topbar-search-input")
 const ogInpStyle = getComputedStyle(searchInput)
+
 let ogInpWidth = parseFloat(ogInpStyle.width.split("px")[0])
 let ogInpHeight = parseFloat(ogInpStyle.height.split("px")[0])
-
 ogInpWidth = (ogInpWidth/window.innerWidth)*100+"vw"
 ogInpHeight = (ogInpHeight/window.innerHeight)*100+"vh"
 
+const progress_bar = id("page-progress-bar")
 
 const searchBar = id("topbar-search-container")
 
@@ -22,3 +25,19 @@ searchInput.addEventListener("blur",function(){
     searchBar.style.width = ogInpWidth
     searchBar.style.height = ogInpHeight
 })
+
+function lerp(a,b,t){
+    return (1-t) * a + t*b
+}
+
+//PROGRESS BAR
+function onScroll(){
+    const topOfPage = document.documentElement.scrollTop
+    const scrollHeight = document.documentElement.scrollHeight-document.documentElement.clientHeight
+    
+    progress_bar.style.width = (topOfPage/scrollHeight)*100+"vw"
+}
+
+window.onscroll = onScroll
+
+onScroll()
---
+++ javascript/lightColors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    ["rgb(55, 55, 55)"]: "rgb(214, 214, 214)",
+    ["rgb(0,0,0)"]: "rgb(255,255,255)",
+}
+
+export const lightBackgroundColor = "rgb(255,255,255)"
